700cc66ddcaed87a254f191be5a707ab233db3ae
sanjeet23j <sanjeet23j@gmail.com>
2019-02-15 13:11:59 +0530

    rounting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,20 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { FormsModule } from '@angular/forms';
 import { EmplTitlePepePipe } from './empl-title-pepe.pipe';
-import { CountEmployeeComponent } from './count-employee/count-employee.component'
+import { CountEmployeeComponent } from './count-employee/count-employee.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SimplePageComponent } from './simple-page/simple-page.component'
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const AppRoutes : Routes = [
+  { path : 'home' , component : HomeComponent  },
+  { path : 'employees', component : EmployeesComponent },
+  { path : '', redirectTo : '/home', pathMatch :'full' },
+  { path : '**', component : PageNotFoundComponent }
+];
 
 
 @NgModule({
@@ -14,12 +27,16 @@ import { CountEmployeeComponent } from './count-employee/count-employee.componen
     AppComponent,
     EmployeesComponent,
     EmplTitlePepePipe,
-    CountEmployeeComponent
+    CountEmployeeComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    SimplePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
